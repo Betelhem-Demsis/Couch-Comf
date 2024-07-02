28c83aa18d3728bbed058f456cacf2292d1afffd
betelhem demsis <bettydemsis@gmail.com>
2024-07-03 00:19:19 +0300

    modified

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "../css/footer.css"
 
 function Footer() {
   return (
-    <div>
+    <div className="footer">
+      <div>
+      <h4>Contact us</h4>
+      <p>+25193829839289</p>
+      </div>
       
+      <div >
+      <h4>Address</h4>
+      <p>AA, dembel mall</p>
+      </div>
+      
+
     </div>
   )
 }
